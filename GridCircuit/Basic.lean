e29511e59265554743f8fe45b1375ecf133f51e3
Weiyi Wang <wwylele@gmail.com>
2026-06-03 15:46:53 -0400

    more

--- GridCircuit/Basic.lean
+++ GridCircuit/Basic.lean
@@ -7,7 +7,7 @@ variable {n : ℕ}
 noncomputable section
 
 -- https://math.stackexchange.com/a/4452978/1197328
-theorem liouville_lemma1 [NeZero n] (f : (Fin n → ℤ) → ℝ)
+theorem liouville_lemma1 (f : (Fin n → ℤ) → ℝ)
     (hharmonic : ∀ x, ∑ k, (f (x - Pi.single k 1) - f x) + ∑ k, (f (x + Pi.single k 1) - f x) = 0)
     (l : ℝ) (e : Fin n)
     (hbound : ∀ x, ∀ k : ℕ, ∑ i ∈ Finset.range k, f (x + i • Pi.single e 1) ≤ l) :
@@ -18,6 +18,7 @@ theorem liouville_lemma1 [NeZero n] (f : (Fin n → ℤ) → ℝ)
     intro x
     specialize hbound x 1
     simpa using hbound
+  have : NeZero n := Fin.neZero e
   have hn0 : 0 < n := NeZero.pos n
   let m : ℝ := sSup (Set.range f)
   have hfm (x : Fin n → ℤ) : f x ≤ m := le_csSup hboundabove (by simp)
@@ -90,7 +91,7 @@ theorem liouville_lemma1 [NeZero n] (f : (Fin n → ℤ) → ℝ)
   rw [← div_le_iff₀ (by simpa using hm)]
   exact (Nat.le_ceil _).trans (by simp)
 
-theorem liouville_lemma1' [NeZero n] (f : (Fin n → ℤ) → ℝ)
+theorem liouville_lemma1' (f : (Fin n → ℤ) → ℝ)
     (hharmonic : ∀ x, ∑ k, (f (x - Pi.single k 1) - f x) + ∑ k, (f (x + Pi.single k 1) - f x) = 0)
     (l : ℝ) (e : Fin n)
     (hbound : ∀ x, ∀ k : ℕ, |∑ i ∈ Finset.range k, f (x + i • Pi.single e 1)| ≤ l) :
@@ -113,20 +114,153 @@ theorem liouville_lemma1' [NeZero n] (f : (Fin n → ℤ) → ℝ)
       rw [neg_le] at h
       simpa using h
 
+theorem liouville_neighbor (f : (Fin n → ℤ) → ℝ)
+    (hharmonic : ∀ x, ∑ k, (f (x - Pi.single k 1) - f x) + ∑ k, (f (x + Pi.single k 1) - f x) = 0)
+    (hbelow : BddBelow (Set.range f)) (habove : BddAbove (Set.range f)) (e : Fin n) :
+    ∀ x, f x = f (x + Pi.single e 1) := by
+  let g := fun x ↦ f (x + Pi.single e 1) - f x
+  suffices g = 0 by
+    unfold g at this
+    rw [funext_iff] at this
+    intro x
+    symm
+    simpa [sub_eq_zero] using this x
+  obtain ⟨a, ha⟩ := habove
+  have ha : ∀ x, f x ≤ a := by simpa [mem_upperBounds] using ha
+  obtain ⟨b, hb⟩ := hbelow
+  have hb : ∀ x, b ≤ f x := by simpa [mem_lowerBounds] using hb
+  refine liouville_lemma1' g ?_ (a - b) e ?_
+  · unfold g
+    intro x
+    conv_lhs =>
+      left
+      conv in fun k ↦ _ =>
+        ext k
+        rw [sub_sub_sub_comm]
+        rw [← add_sub_right_comm]
+      rw [Finset.sum_sub_distrib]
+    conv_lhs =>
+      right
+      conv in fun k ↦ _ =>
+        ext k
+        rw [sub_sub_sub_comm]
+        rw [← add_right_comm]
+      rw [Finset.sum_sub_distrib]
+    rw [sub_add_sub_comm, hharmonic, hharmonic, sub_zero]
+  · intro x k
+    unfold g
+    have (i : ℕ) : x + i • Pi.single e 1 + Pi.single e 1 = x + (i + 1) • Pi.single e 1 := by ring
+    simp_rw [this]
+    rw [Finset.sum_range_sub (fun i ↦ f (x + i • (Pi.single e 1 : Fin n → ℤ))) k]
+    rw [abs_le', neg_sub]
+    constructor <;> apply sub_le_sub (ha _) (hb _)
 
+theorem liouville_line (f : (Fin n → ℤ) → ℝ)
+    (hharmonic : ∀ x, ∑ k, (f (x - Pi.single k 1) - f x) + ∑ k, (f (x + Pi.single k 1) - f x) = 0)
+    (hbelow : BddBelow (Set.range f)) (habove : BddAbove (Set.range f)) (e : Fin n) (l : ℤ) :
+    ∀ x, f x = f (x + Pi.single e l) := by
+  induction l with
+  | zero => simp
+  | succ l h =>
+    intro x
+    rw [liouville_neighbor f hharmonic hbelow habove e x, h (x + Pi.single e 1), add_right_comm,
+      add_assoc, Pi.single_add]
+  | pred l h =>
+    intro x
+    rw [h x]
+    rw [liouville_neighbor f hharmonic hbelow habove e (x + (Pi.single e (-↑l - 1) : Fin n → ℤ))]
+    rw [add_assoc, ← Pi.single_add, sub_add_cancel]
+
+theorem liouville (f : (Fin n → ℤ) → ℝ)
+    (hharmonic : ∀ x, ∑ k, (f (x - Pi.single k 1) - f x) + ∑ k, (f (x + Pi.single k 1) - f x) = 0)
+    (hbelow : BddBelow (Set.range f)) (habove : BddAbove (Set.range f)) :
+    ∀ x y, f x = f y := by
+  suffices ∀ x y, f x = f (x + y) by
+    intro x y
+    simpa using this x (y - x)
+  intro x y
+  induction y using Pi.single_induction generalizing x with
+  | zero => simp
+  | single a l => apply liouville_line f hharmonic hbelow habove
+  | add a b ha hb => rw [ha x, hb (x + a), add_assoc]
 
 structure IsElectricPotential (cur : (Fin n → ℤ) → ℝ) (pot : (Fin n → ℤ) → ℝ) : Prop where
   kirchhoff (x : Fin n → ℤ) :
     ∑ k, (pot (x - Pi.single k 1) - pot x) + ∑ k, (pot (x + Pi.single k 1) - pot x) = cur x
-  boundary : Tendsto pot (Bornology.cobounded (Fin n → ℤ)) (𝓝 0)
+  bddBelow : BddBelow (Set.range pot)
+  bddAbove : BddAbove (Set.range pot)
 
-theorem isElectricPotential_zero_iff (pot : (Fin n → ℤ) → ℝ) :
-    IsElectricPotential 0 pot ↔ pot = 0 where
-  mp := sorry
-  mpr := sorry
+theorem isElectricPotential_zero [NeZero n] {pot : (Fin n → ℤ) → ℝ} :
+    IsElectricPotential 0 pot ↔ ∃ c, pot = fun _ ↦ c where
+  mp h := by
+    have hconstant : pot = fun _ ↦ pot 0 := by
+      ext x
+      exact liouville pot h.kirchhoff h.bddBelow h.bddAbove x 0
+    use pot 0
+  mpr h := by
+    obtain ⟨c, rfl⟩ := h
+    exact {
+      kirchhoff := by simp
+      bddBelow := ⟨c, by simp⟩
+      bddAbove := ⟨c, by simp⟩
+    }
 
-def cur (x : Fin n → ℤ) : ℝ := Pi.single (M := fun (_ : Fin n → ℤ) ↦ ℝ) 0 1 x
+theorem bddBelow_range_sub {α : Type*} {β : Type*} [LinearOrder β] [AddCommGroup β]
+    [IsOrderedAddMonoid β]
+    {a b : α → β} (ha : BddBelow (Set.range a)) (hb : BddAbove (Set.range b)) :
+    BddBelow (Set.range (a - b)) := by
+  obtain ⟨ca, ha⟩ := ha
+  obtain ⟨cb, hb⟩ := hb
+  use ca - cb
+  suffices ∀ (x : α), ca - cb ≤ a x - b x by simpa [mem_lowerBounds]
+  have ha : ∀ (x : α), ca ≤ a x:= by simpa [mem_lowerBounds] using ha
+  have hb : ∀ (x : α), b x ≤ cb := by simpa [mem_upperBounds] using hb
+  exact fun x ↦ sub_le_sub (ha x) (hb x)
 
+theorem bddAbove_range_sub {α : Type*} {a b : α → ℝ} (ha : BddAbove (Set.range a))
+    (hb : BddBelow (Set.range b)) :
+    BddAbove (Set.range (a - b)) := by
+  obtain ⟨ca, ha⟩ := ha
+  obtain ⟨cb, hb⟩ := hb
+  use ca - cb
+  suffices ∀ (x : α), a x - b x ≤ ca - cb by simpa [mem_upperBounds]
+  have ha : ∀ (x : α), a x ≤ ca := by simpa [mem_upperBounds] using ha
+  have hb : ∀ (x : α), cb ≤ b x := by simpa [mem_lowerBounds] using hb
+  exact fun x ↦ sub_le_sub (ha x) (hb x)
+
+theorem isElectrictPotential_unique [NeZero n] {cur : (Fin n → ℤ) → ℝ} {a b : (Fin n → ℤ) → ℝ}
+    (ha : IsElectricPotential cur a) (hb : IsElectricPotential cur b) :
+    ∃ c, a - b = fun _ ↦ c := by
+  rw [← isElectricPotential_zero]
+  constructor
+  · intro x
+    convert congr($(ha.kirchhoff x) - $(hb.kirchhoff x)) using 1
+    · rw [add_sub_add_comm, ← Finset.sum_sub_distrib, ← Finset.sum_sub_distrib]
+      congr
+      all_goals
+      · ext x
+        simp
+        ring
+    · simp
+  · exact bddBelow_range_sub ha.bddBelow hb.bddAbove
+  · exact bddAbove_range_sub ha.bddAbove hb.bddBelow
+
+def unitCur (c x : Fin n → ℤ) : ℝ := Pi.single (M := fun (_ : Fin n → ℤ) ↦ ℝ) c 1 x
+
+open Classical in
+def equivResistance (x : Fin n → ℤ) : Option ℝ :=
+  if h : ∃ pot, IsElectricPotential (unitCur x - unitCur 0) pot then
+    some <| h.choose x - h.choose 0
+  else
+    none
+
+theorem equivResistance_eq [NeZero n] {x : Fin n → ℤ} {pot : (Fin n → ℤ) → ℝ}
+    (h : IsElectricPotential (unitCur x - unitCur 0) pot) :
+    equivResistance x = some (pot x - pot 0) := by
+  have h' : ∃ pot, IsElectricPotential (unitCur x - unitCur 0) pot := ⟨pot, h⟩
+  obtain ⟨c, hc⟩ := isElectrictPotential_unique h'.choose_spec h
+  rw [sub_eq_iff_eq_add] at hc
+  simp [equivResistance, h', hc]
 
 theorem Pi.intCast_single (a : Fin n) (b : ℤ) (x : Fin n) :
     ((Pi.single (M := fun (_ : Fin n) ↦ ℤ) a b x) : ℝ) =
@@ -140,11 +274,11 @@ theorem Pi.single_mul_left_const_apply {ι : Type*} {α : Type*}
     single (M := fun _ ↦ α) i (a * f) j = single (M := fun _ ↦ α) i a j * f := by
   by_cases h : i = j <;> aesop
 
-theorem cur_eq (x : Fin n → ℤ) :
-    cur x = (2 * π)⁻¹ ^ n *
-    ∫ (w : Fin n → ℝ) in Set.Icc 0 (fun _ ↦ 2 * π), cos (∑ i, x i * w i) := by
-  suffices cur x = (2 * π)⁻¹ ^ n *
-      RCLike.re (∫ (w : Fin n → ℝ) in Set.Icc 0 (fun _ ↦ 2 * π),
+theorem fourier_unitCur (x : Fin n → ℤ) :
+    unitCur 0 x = (2 * π)⁻¹ ^ n *
+    ∫ (w : Fin n → ℝ) in Set.Icc (fun _ ↦ -π) (fun _ ↦ π), cos (∑ i, x i * w i) := by
+  suffices unitCur 0 x = (2 * π)⁻¹ ^ n *
+      RCLike.re (∫ (w : Fin n → ℝ) in Set.Icc (fun _ ↦ -π) (fun _ ↦ π),
       Complex.exp ((∑ i, x i * w i : ℝ) * Complex.I)) by
     rw [this]
     rw [← integral_re ?_]
@@ -157,22 +291,22 @@ theorem cur_eq (x : Fin n → ℤ) :
   rw [integral_fintype_prod_eq_prod
     (fun (i : Fin n) (w : ℝ) ↦ Complex.exp ((x i) * (w : ℂ) * Complex.I))]
   by_cases h0 : x = 0
-  · have hmax : max (2 * π) 0 = 2 * π := by simpa using pi_nonneg
-    have hpow : ((2 * π : ℂ) ^ n).re = (2 * π) ^ n := by
+  · have hmax : max (π + π) 0 = 2 * π := by simpa [two_mul] using pi_nonneg
+    have hpow : ((2 * π  : ℂ) ^ n).re = (2 * π) ^ n := by
       trans (2 * π : ℝ) ^ (n : ℝ)
       · rw [Real.rpow_def]
         simp
       simp
-    simp [cur, h0, hmax, hpow, ← mul_pow]
+    simp [unitCur, h0, hmax, hpow, ← mul_pow]
     field_simp
     simp
-  · suffices ∏ x_1, ∫ w  in Set.Icc 0 (2 * π), Complex.exp (x x_1 * w * Complex.I) = 0 by
-      simp [cur, h0, this]
+  · suffices ∏ x_1, ∫ w in Set.Icc (-π) π, Complex.exp (x x_1 * w * Complex.I) = 0 by
+      simp [unitCur, h0, this]
     simp_rw [funext_iff, Pi.zero_apply, not_forall] at h0
     obtain ⟨k, hk⟩ := h0
     apply Finset.prod_eq_zero (Finset.mem_univ k)
     rw [integral_Icc_eq_integral_Ioc, ← intervalIntegral.integral_of_le (by simpa using pi_nonneg)]
-    have hderiv : ∀ w ∈ Set.uIcc 0 (2 * π), HasDerivAt
+    have hderiv : ∀ w ∈ Set.uIcc (-π) π, HasDerivAt
         (fun (w : ℝ) ↦ Complex.exp (x k * w * Complex.I) * (x k * Complex.I)⁻¹)
         ((fun (w : ℝ) ↦ Complex.exp (x k * w * Complex.I)) w) w := by
       intro w _
@@ -191,12 +325,17 @@ theorem cur_eq (x : Fin n → ℤ) :
       exact hasDerivAt_id' w
     rw [intervalIntegral.integral_eq_sub_of_hasDerivAt hderiv
       (Continuous.intervalIntegrable (by fun_prop) _ _)]
-    rw [mul_assoc (x k : ℂ)]
-    simp
-
+    rw [← sub_mul]
+    apply mul_eq_zero_of_left
+    rw [sub_eq_zero]
+    apply eq_of_div_eq_one
+    rw [← Complex.exp_sub]
+    convert Complex.exp_int_mul_two_pi_mul_I (x k) using 2
+    push_cast
+    ring
 
 def φ (x : Fin n → ℤ) : ℝ :=
-  (2 * π)⁻¹ ^ n * ∫ (w : Fin n → ℝ) in Set.Icc 0 (fun _ ↦ 2 * π),
+  (2 * π)⁻¹ ^ n * ∫ (w : Fin n → ℝ) in Set.Icc (fun _ ↦ -π) (fun _ ↦ π),
     (1 - Real.cos (∑ i, x i * w i)) / ∑ i, (2 - 2 * Real.cos (w i))
 
 -- Aristotle
@@ -250,24 +389,27 @@ theorem sin_inequality (x : Fin n → ℤ) (y : Fin n → ℝ) :
 
 theorem integrable_φ [NeZero n] (x : Fin n → ℤ) :
     IntegrableOn (fun w ↦ (1 - cos (∑ k, x k * w k)) / ∑ k, (2 - 2 * cos (w k)))
-      (Set.Icc 0 (fun _ ↦ 2 * π)) := by
-  rw [IntegrableOn, ← Set.pi_univ_Icc, volume_pi, Measure.restrict_pi_pi]
-  simp_rw [← restrict_Ioo_eq_restrict_Icc]
-  rw [← Measure.restrict_pi_pi]
+      (Set.Icc (fun _ ↦ -π) (fun _ ↦ π)) := by
+  have hm : MeasurableSet (Set.Icc (fun x ↦ -π) fun x ↦ π : Set (Fin n → ℝ)) := by
+    rw [← Set.pi_univ_Icc]
+    apply MeasurableSet.pi Set.countable_univ fun i _ ↦ measurableSet_Icc
   refine IntegrableOn.of_bound ?_ ?_ (2⁻¹ * ∑ k, (x k : ℝ) ^ 2) ?_
-  · simp [NeZero.ne n]
-    finiteness
-  · refine ContinuousOn.aestronglyMeasurable ?_
-      (MeasurableSet.pi Set.countable_univ fun i _ ↦ measurableSet_Ioo)
+  · simp [Real.volume_Icc_pi]
+  · rw [← Measure.restrict_inter_add_diff _ (measurableSet_singleton 0)]
+    rw [Set.inter_eq_right.mpr (by simp [Pi.le_def, pi_nonneg])]
+    rw [Measure.restrict_singleton', zero_add]
+    refine ContinuousOn.aestronglyMeasurable ?_ (hm.diff (measurableSet_singleton 0))
     refine ContinuousOn.div₀ (by fun_prop) (by fun_prop) ?_
     intro i hi
-    refine (Finset.sum_pos (fun j hj ↦ ?_) (by simp)).ne'
-    specialize hi j (Set.mem_univ j)
-    suffices cos (i j) < 1 by simpa
-    apply lt_of_le_of_ne (cos_le_one _)
-    rw [ne_eq, cos_eq_one_iff_of_lt_of_lt (lt_trans (by simp [pi_pos]) hi.1) hi.2]
-    exact hi.1.ne'
-  apply ae_restrict_of_forall_mem (MeasurableSet.pi Set.countable_univ fun i _ ↦ measurableSet_Ioo)
+    simp only [Set.mem_diff, Set.mem_Icc, Set.mem_singleton_iff] at hi
+    obtain ⟨⟨hi1, hi2⟩, hi3⟩ := hi
+    contrapose! hi3 with hj
+    rw [Finset.sum_eq_zero_iff_of_nonneg (fun j _ ↦ by simpa using cos_le_one (i j))] at hj
+    ext j
+    rw [Pi.zero_apply, ← cos_eq_one_iff_of_lt_of_lt ((lt_of_lt_of_le (by grind [pi_pos]) (hi1 j)))
+      ((hi2 j).trans_lt (by grind [pi_pos]))]
+    simpa [sub_eq_zero] using hj j (Finset.mem_univ j)
+  apply ae_restrict_of_forall_mem hm
   intro w hw
   rw [norm_div]
   apply div_le_of_le_mul₀ (by simp) (by positivity)
@@ -281,10 +423,10 @@ theorem integrable_φ [NeZero n] (x : Fin n → ℤ) :
   apply le_of_eq
   ring
 
-theorem φ_equation [NeZero n] (x : Fin n → ℤ) :
-    (∑ k, (φ (x - Pi.single k 1) + φ (x + Pi.single k 1))) - 2 * n * φ x = cur x := by
-  rw [show 2 * n * φ x = ∑ k : Fin n, (2 * φ x) by simp; ring]
-  rw [← Finset.sum_sub_distrib]
+theorem φ_kirchhoff [NeZero n] (x : Fin n → ℤ) :
+    ∑ k, (φ (x - Pi.single k 1) - φ x) + ∑ k, (φ (x + Pi.single k 1) - φ x) = unitCur 0 x := by
+  rw [← Finset.sum_add_distrib]
+  simp_rw [sub_add_sub_comm, ← two_mul]
   unfold φ
   conv in ∑ k, _ =>
     right; ext k
@@ -314,7 +456,7 @@ theorem φ_equation [NeZero n] (x : Fin n → ℤ) :
     rw [← Finset.mul_sum, ← Finset.sum_div]
   have hcongr (f : (Fin n → ℝ) → ℝ) :
       (fun w ↦ f w * ((∑ x, (2 - 2 * cos (w x))) / ∑ x, (2 - 2 * cos (w x))))
-      =ᵐ[volume.restrict (Set.Icc 0 (fun _ ↦ 2 * π))] f := by
+      =ᵐ[volume.restrict (Set.Icc (fun _ ↦ -π) (fun _ ↦ π))] f := by
     refine EventuallyEq.filter_mono ?_ ae_restrict_le
     suffices ∀ᵐ w : Fin n → ℝ, ∀ z : Fin n → ℤ, w ≠ fun k ↦ z k * (2 * π) by
       unfold EventuallyEq
@@ -331,4 +473,56 @@ theorem φ_equation [NeZero n] (x : Fin n → ℤ) :
     intro z
     exact Measure.ae_ne volume fun k ↦ z k * (2 * π)
   rw [integral_congr_ae (hcongr _)]
-  rw [← cur_eq]
+  rw [← fourier_unitCur]
+
+theorem isElectricPotential_φ [NeZero n] (a b : Fin n → ℤ) :
+    IsElectricPotential (unitCur a - unitCur b) (fun x ↦ φ (x - a) - φ (x - b)) where
+  kirchhoff x := by
+    conv_lhs =>
+      left
+      conv in fun k ↦ _ =>
+        ext k
+        rw [sub_sub_sub_comm]
+        rw [sub_right_comm _ _ a]
+        rw [sub_right_comm _ _ b]
+      rw [Finset.sum_sub_distrib]
+    conv_lhs =>
+      right
+      conv in fun k ↦ _ =>
+        ext k
+        rw [sub_sub_sub_comm]
+        rw [add_sub_right_comm]
+        rw [add_sub_right_comm]
+      rw [Finset.sum_sub_distrib]
+    rw [sub_add_sub_comm, φ_kirchhoff, φ_kirchhoff, Pi.sub_apply]
+    unfold unitCur
+    congrm ?_ - ?_
+    · by_cases h : x = a
+      · simp [h]
+      · have : x - a ≠ 0 := sub_eq_zero.ne.mpr h
+        simp [h, this]
+    · by_cases h : x = b
+      · simp [h]
+      · have : x - b ≠ 0 := sub_eq_zero.ne.mpr h
+        simp [h, this]
+  bddAbove := sorry
+  bddBelow := sorry
+  /-boundary := by
+    unfold φ
+    conv in fun x ↦ _ =>
+      ext x
+      rw [← mul_sub]
+      rw [← integral_sub sorry sorry]
+      conv in fun w ↦ _ - _ =>
+        ext w
+        rw [← sub_div, sub_sub_sub_cancel_left, cos_sub_cos, ← Finset.sum_add_distrib,
+          ← Finset.sum_sub_distrib]
+        conv in fun k ↦ _ * _ + _ * _ =>
+          ext k
+          rw [← add_mul, ← Int.cast_add, ← Pi.add_apply, sub_add_sub_comm, ← two_mul]
+        conv in fun k ↦ _ * _ - _ * _ =>
+          ext k
+          rw [← sub_mul, ← Int.cast_sub, ← Pi.sub_apply, sub_sub_sub_cancel_left]
+
+    sorry
+-/
